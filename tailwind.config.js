2a3739a3ac5f970a0f2b62904ef70b4ece4cb8a9
camilla <camillaalves285@gmail.com>
2024-07-02 21:03:29 -0300

    chore: Adicionei o Tailwind, Prettier e Eslint ao projeto.

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./*.html",
+    "./index.html",
+    "./src/**/*.{html,js,jsx}",
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+    "./app/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
