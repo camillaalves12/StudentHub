58e09bcab1bef61781d91461b1337e2de7b4b8d0
camilla <camillaalves285@gmail.com>
2024-07-07 18:19:50 -0300

    docs: Modifiquei algumas regras de estilização.

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import './App.css'
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold text-red-600 underline">Testando </h1>
+      <h1 className="text-3xl font-bold text-red-600 underline">
+        Test: Student Hub
+      </h1>
     </>
   )
 }
