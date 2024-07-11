7ac5d8434f959d5870538ebda02a784f08ae5dcd
camilla <camillaalves285@gmail.com>
2024-07-10 21:22:59 -0300

    test: adicionei alguns testes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import './App.css'
+import Home from './components/Home/Home'
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold text-red-600 underline">
-        Test: Student Hub
-      </h1>
+      <Home />
     </>
   )
 }
