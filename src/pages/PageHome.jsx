5b5fa12338607fe7415a1d279a180dbf7850afb3
camilla <camillaalves285@gmail.com>
2024-07-12 12:29:51 -0300

    feat: Adicionei o Page Home.

---
+++ src/pages/PageHome.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../components/Header/Header'
+import Home from '../components/Home/Home'
+
+export default function PageHome() {
+  return (
+    <div>
+      <Header />
+      <Home />
+    </div>
+  )
+}
