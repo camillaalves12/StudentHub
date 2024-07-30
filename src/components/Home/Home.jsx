ea8aee0b98bcead5411d8072d56312105258dadf
camilla <camillaalves285@gmail.com>
2024-07-30 10:55:23 -0300

    style: modifiquei o tamanho dos textos e da imagem.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex h-full items-center justify-center bg-zinc-800 px-20">
       <div className="flex w-full max-w-full items-center justify-between gap-10 px-4">
-        <div className="clip-polygon w-2/5">
+        <div className="clip-polygon w-3/5">
           {' '}
           {/* 40% para a imagem */}
           <img
@@ -14,22 +14,21 @@ export default function Home() {
           />
         </div>
 
-        <div className="flex w-3/5 flex-col justify-start">
+        <div className="flex w-4/5 flex-col justify-start">
           {' '}
           {/* 60% para o texto */}
-          <p className="w-full pb-5 text-5xl font-bold text-orange-800">
+          <p className="w-full pb-5 text-7xl font-bold text-orange-800">
             Começar a sua graduação nunca foi tão simples
           </p>
-          <p className="w-full pb-5 text-4xl font-bold leading-relaxed text-white">
-            Vem pra Anhanguera e dê o start na carreira por{' '}
-            <span className="rounded-md bg-orange-700 p-1">R$ 59*</span> pelo
-            Pague Fácil!
+          <p className="w-full pb-5 text-5xl font-bold leading-relaxed text-white">
+            Vem pra Student Hub e dê o start na sua carreira a partir de{' '}
+            <span className="rounded-md bg-orange-700 p-1">R$ 59*</span>
           </p>
-          <div className="flex gap-10">
-            <button className="mt-5 rounded-3xl bg-orange-800 px-6 py-2 font-semibold text-white hover:bg-orange-700">
+          <div className="mt-5 flex gap-10">
+            <button className="rounded-3xl bg-orange-800 px-6 py-4 text-xl font-semibold text-white hover:bg-orange-700">
               EXPLORAR CURSOS
             </button>
-            <button className="mt-5 rounded-3xl bg-orange-800 px-6 py-2 font-semibold text-white hover:bg-orange-700">
+            <button className="rounded-3xl bg-orange-800 px-6 py-4 text-xl font-semibold text-white hover:bg-orange-700">
               INSCREVA-SE
             </button>
           </div>
