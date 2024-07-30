eb23326c9542d909c88790654f5b2b3e78d6bf02
camilla <camillaalves285@gmail.com>
2024-07-30 16:42:42 -0300

    style: modifiquei o tamanho do card.

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -10,21 +10,21 @@ export default function Card({
   shift,
 }) {
   return (
-    <div className="flex size-80 flex-col justify-center gap-4 rounded-3xl border-2 border-white p-4 text-white">
+    <div className="flex h-96 w-80 flex-col justify-center gap-7 rounded-3xl border-2 border-white p-4 text-white">
       <div>
-        <p className="text-xl font-bold">{course}</p>
-        <p className="text-base">
+        <p className="text-2xl font-bold">{course}</p>
+        <p className="text-lg">
           {typeCourse} - {numberPeriod} Períodos
         </p>
       </div>
-      <p className="mt-3 w-5/12 rounded-3xl border-2 border-orange-700  text-center text-base font-semibold text-white hover:bg-orange-700">
+      <p className="mt-3 w-5/12 rounded-3xl border-2 border-orange-700  text-center text-lg font-semibold text-white hover:bg-orange-700">
         {modality}
       </p>
       <div>
-        <p className="text-2xl font-bold">R$ {price}</p>
-        <p className="text-base">Turno: {shift} </p>
+        <p className="text-3xl font-bold">R$ {price}</p>
+        <p className="text-lg">Turno: {shift} </p>
       </div>
-      <button className="mt-3 rounded-3xl border-2 border-orange-700  px-10 py-1 font-semibold text-white hover:bg-orange-700">
+      <button className="mt-3 rounded-3xl border-2 border-orange-700 px-10 py-1 text-lg font-semibold text-white hover:bg-orange-700">
         INSCREVA-SE
       </button>
     </div>
