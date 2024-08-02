7fce0fe492931c34dc87449e4bb2337ce99ed8b9
camilla <camillaalves285@gmail.com>
2024-08-02 10:56:51 -0300

    feat: Criei uma nova página na aplicação, a do Cards das areas.

---
+++ src/pages/PageCardArea.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import CardArea from '../components/CardArea/CardArea'
+
+export default function PageCardArea() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-zinc-800 px-20">
+      <CardArea />
+    </div>
+  )
+}
