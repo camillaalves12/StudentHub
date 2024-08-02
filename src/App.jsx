0b9094e8241b77ab128b64f49267facafae04c10
camilla <camillaalves285@gmail.com>
2024-08-02 10:51:43 -0300

    feat: Adicionei uma nova Page, a dos Card das areas.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './App.css'
 import PageHome from './pages/PageHome'
 import PageCards from './pages/PageCards'
+import PageCardArea from './pages/PageCardArea'
 
 function App() {
   return (
     <div className="flex-col">
       <PageHome />
       <PageCards />
+      <PageCardArea />
     </div>
   )
 }
