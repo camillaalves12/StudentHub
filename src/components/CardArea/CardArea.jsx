ac4942023bf07a584ded85f59301f07d9371bd9c
camilla <camillaalves285@gmail.com>
2024-08-04 15:36:49 -0300

    style: modifiquei o tamanho da imagem e da container.

--- src/components/CardArea/CardArea.jsx
+++ src/components/CardArea/CardArea.jsx
@@ -2,19 +2,27 @@ import React from 'react'
 
 export default function CardArea() {
   return (
-    <div className="box-border flex h-96 w-80 flex-col justify-center gap-7 rounded-3xl border-2 border-white p-4 text-white">
+    <div className="box-border flex h-auto w-80 flex-col gap-3 rounded-3xl border-2 border-white text-white">
       <img
-        className="h-auto w-full rounded-t-lg"
+        className="w-full rounded-t-3xl"
         src="https://img.freepik.com/fotos-gratis/medico-e-paciente-alegre-com-papeis_23-2147763774.jpg?ga=GA1.1.1125524108.1713916092&semt=ais_hybrid"
         alt="Imagem do Card"
       />
-      <p className="text-3xl font-bold text-orange-700">Saúde</p>
-      <p className="box-border cursor-pointer border-b-2 border-slate-600 py-2">
-        Enfermagem
-      </p>
-      <p className="box-border cursor-pointer border-b-2 border-slate-600 py-2">
-        Nutrição
-      </p>
+      <div className="p-4 pb-5">
+        <p className="text-3xl font-bold text-orange-700">Saúde</p>
+        <p className="box-border cursor-pointer border-b-2 border-slate-600 py-3 hover:bg-slate-700 hover:font-bold">
+          Enfermagem
+        </p>
+        <p className="box-border cursor-pointer border-b-2 border-slate-600 py-3  hover:bg-slate-700 hover:font-bold">
+          Medicina
+        </p>
+        <p className="box-border cursor-pointer border-b-2 border-slate-600 py-3">
+          Nutrição
+        </p>
+        <p className="box-border cursor-pointer border-b-2 border-slate-600 py-3">
+          Odontologia
+        </p>
+      </div>
     </div>
   )
 }
