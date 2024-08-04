fd7b4edd320d5fbcb726052cacbeb35577b10cc7
camilla <camillaalves285@gmail.com>
2024-08-04 15:37:47 -0300

    feat: criei o page que irá renderizar os cards das areas.

--- src/pages/PageCardArea.jsx
+++ src/pages/PageCardArea.jsx
@@ -3,8 +3,16 @@ import CardArea from '../components/CardArea/CardArea'
 
 export default function PageCardArea() {
   return (
-    <div className="flex h-screen items-center justify-center bg-zinc-800 px-20">
-      <CardArea />
+    <div className="flex h-screen flex-col items-center justify-center gap-3 bg-zinc-800">
+      <p className="mt-4 py-9 text-3xl font-bold text-orange-800">
+        Confira os cursos por área de conhecimento
+      </p>
+      <div className="flex flex-wrap gap-10">
+        <CardArea />
+        <CardArea />
+        <CardArea />
+        <CardArea />
+      </div>
     </div>
   )
 }
