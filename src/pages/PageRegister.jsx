dab37acadcfcf40cef4ecad0c1af780a62a55b44
camilla <camillaalves285@gmail.com>
2024-08-15 14:22:02 -0300

    style: modifiquei o tamanho da página.

--- src/pages/PageRegister.jsx
+++ src/pages/PageRegister.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Register from '../components/Register/Register'
+import Header from '../components/Header/Header'
 
 export default function PageRegister() {
   return (
-    <div className="flex h-[calc(100vh-68px)] items-center justify-center gap-12 bg-white">
+    <div className="flex h-screen items-center justify-center gap-12 bg-white">
+      <Header />
       <div className="w-3/12">
         <p className="text-right text-4xl font-semibold leading-snug text-orange-700">
           INSCREVA-SE COM O MELHOR PREÇO E DESCONTOS IMPERDÍVEIS
