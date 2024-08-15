11a3802062660cac8a328038bc24d4ca83c1ce65
camilla <camillaalves285@gmail.com>
2024-08-15 11:12:40 -0300

    style: modifiquei o tamanho da página, utlizei o calc.

--- src/pages/PageCardArea.jsx
+++ src/pages/PageCardArea.jsx
@@ -3,11 +3,11 @@ import CardArea from '../components/CardArea/CardArea'
 
 export default function PageCardArea() {
   return (
-    <div className="flex h-screen flex-col items-center justify-center gap-3 bg-zinc-800">
-      <p className="mt-4 py-9 text-3xl font-bold text-orange-800">
+    <div className="flex h-[calc(100vh-68px)] flex-col items-center justify-center gap-3 bg-zinc-800">
+      <p className="py-5 text-3xl font-bold text-orange-800">
         Confira os cursos por área de conhecimento
       </p>
-      <div className="flex flex-wrap gap-10">
+      <div className="flex  gap-10">
         <CardArea />
         <CardArea />
         <CardArea />
