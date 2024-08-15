3016b8e3f5e5707e16dbaf2cf8b1af0805333b2a
camilla <camillaalves285@gmail.com>
2024-08-15 11:12:17 -0300

    style: modifiquei o tamanho da página, utlizei o calc.

--- src/pages/PageRegister.jsx
+++ src/pages/PageRegister.jsx
@@ -3,7 +3,7 @@ import Register from '../components/Register/Register'
 
 export default function PageRegister() {
   return (
-    <div className="flex h-screen items-center justify-center gap-10 bg-white">
+    <div className="flex h-[calc(100vh-68px)] items-center justify-center gap-12 bg-white">
       <div className="w-3/12">
         <p className="text-right text-4xl font-semibold leading-snug text-orange-700">
           INSCREVA-SE COM O MELHOR PREÇO E DESCONTOS IMPERDÍVEIS
