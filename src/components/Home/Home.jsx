47017d32c6746f9e1de97758586520751c90c56b
camilla <camillaalves285@gmail.com>
2024-08-15 14:45:50 -0300

    feat: adicionei ao btn a navegação para a página das areas dos cursos.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,26 @@
 import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { scroller } from 'react-scroll'
 
 export default function Home() {
+  const navigate = useNavigate()
+
+  const handleNavigation = (section) => {
+    const scrollOptions = {
+      duration: 400,
+      delay: 0,
+      smooth: 'easeInOutQuart',
+    }
+
+    if (window.location.pathname !== '/') {
+      navigate('/')
+      setTimeout(() => {
+        scroller.scrollTo(section, scrollOptions)
+      }, 100) // Ajuste o tempo de delay conforme necessário
+    } else {
+      scroller.scrollTo(section, scrollOptions)
+    }
+  }
   return (
     <div className="flex h-screen items-center justify-center bg-zinc-800 px-20">
       <div className="flex w-full max-w-full items-center justify-between gap-10 px-4">
@@ -25,11 +45,14 @@ export default function Home() {
             <span className="rounded-md bg-orange-700 p-1">R$ 59*</span>
           </p>
           <div className="mt-5 flex gap-10">
-            <button className="rounded-3xl bg-orange-700 p-3 text-base font-bold text-white hover:bg-orange-800">
+            <button
+              onClick={() => handleNavigation('coursesArea')}
+              className="rounded-3xl bg-orange-700 p-3 text-base font-bold text-white hover:bg-orange-800"
+            >
               EXPLORAR CURSOS
             </button>
             <button className="rounded-3xl bg-orange-700 p-3 text-base font-bold text-white hover:bg-orange-800">
-              INSCREVA-SE
+              <Link to="/register">INSCREVA-SE</Link>
             </button>
           </div>
         </div>
