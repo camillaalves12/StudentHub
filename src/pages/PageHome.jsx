8bd7b1ef943e80951178b6403f560f5546fb0445
camilla <camillaalves285@gmail.com>
2024-08-15 14:29:34 -0300

    feat: adicionei as outras páginas na pageHome.

--- src/pages/PageHome.jsx
+++ src/pages/PageHome.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import Header from '../components/Header/Header'
 import Home from '../components/Home/Home'
+import PageCards from './PageCards'
+import PageCardArea from './PageCardArea'
 
 export default function PageHome() {
   return (
-    <div className="flex h-screen flex-col">
+    <div className="flex flex-col">
       <Header />
-      <div className="grow">
+      <div id="home">
         <Home />
+      </div>
+      <div id="courses">
+        <PageCards />
+      </div>
+      <div id="coursesArea">
+        <PageCardArea />
       </div>
     </div>
   )
