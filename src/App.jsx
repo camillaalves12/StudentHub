340071c2a40ebef273a34234e653a69e17249635
camilla <camillaalves285@gmail.com>
2024-08-15 14:33:44 -0300

    feat: adicionei a navegação entre as páginas.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import React from 'react'
-import './App.css'
+// import './App.css'
 import PageHome from './pages/PageHome'
-import PageCards from './pages/PageCards'
-import PageCardArea from './pages/PageCardArea'
+// import PageCards from './pages/PageCards'
+// import PageCardArea from './pages/PageCardArea'
 import PageRegister from './pages/PageRegister'
+// import Course from './components/Course/Course'
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+// import Course from './components/Course/Course'
 
 function App() {
   return (
     <div className="flex-col">
-      <PageHome />
-      <PageCards />
-      <PageCardArea />
-      <PageRegister />
+      <Router>
+        <Routes>
+          <Route path="/" element={<PageHome />} />
+          {/* <Route path="/courses" element={<PageCards />} /> */}
+          {/* <Route path="/course" element={<Course />} /> */}
+          <Route path="/register" element={<PageRegister />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
